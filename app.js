53cec624838e92da3b1430264c7ca0d26ee23077
Muhammad Saad <muhammadsaad6345@gmail.com>
2022-07-17 14:35:14 +0500

    FlattenDeep in app.js added

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const pkg = require('lodash');
 //array of arrays 
 const items = [1,[2,1,[3,2,[4,2]]]]
 
+ans = pkg.flattenDeep(items);
+console.log(ans)
 
 console.log("Helo Russia world in real terms");
 console.time()
